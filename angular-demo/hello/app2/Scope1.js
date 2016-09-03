77f46d241edb61d0019abac521e9d48ac4daa0ac
dunizb <dunizb@outlook.com>
2016-09-03 16:04:36 +0800

    angularjs-test-app

--- angular-demo/hello/app2/Scope1.js
+++ angular-demo/hello/app2/Scope1.js
@@ -1,8 +1,17 @@
-function GreetCtrl($scope, $rootScope) {
-	$scope.name = 'World';
-	$rootScope.department = 'Angular';
-}
+angular.module("myApp",[])
+	.controller("GreetCtrl",["$scope","$rootScope",function($scope,$rootScope){
+		$scope.name = 'World';
+		$rootScope.department = 'Angular';
+	}])
+	.controller("ListCtrl",["$scope",function($scope){
+		$scope.names = ['Igor', 'Misko', 'Vojta'];
+	}])
 
-function ListCtrl($scope) {
-	$scope.names = ['Igor', 'Misko', 'Vojta'];
-}
+//function GreetCtrl($scope, $rootScope) {
+//	$scope.name = 'World';
+//	$rootScope.department = 'Angular';
+//}
+//
+//function ListCtrl($scope) {
+//	$scope.names = ['Igor', 'Misko', 'Vojta'];
+//}
