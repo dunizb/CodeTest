05214ef86d261c49e0d92b51bff4728c09616967
dunizb <dunizb@outlook.com>
2016-09-03 20:10:57 +0800

    angularjs-test-app

--- angular-demo/hello/app2/Scope2.js
+++ angular-demo/hello/app2/Scope2.js
@@ -1,6 +1,14 @@
-function EventController($scope) {
-	$scope.count = 0;
-	$scope.$on('MyEvent', function() {
-		$scope.count++;
-	});
-}
+angular.module("myApp",[])
+	.controller("EventController",["$scope",function($scope){
+		$scope.count = 0;
+		$scope.$on('MyEvent', function() {
+			$scope.count++;
+		});
+	}])
+
+//function EventController($scope) {
+//	$scope.count = 0;
+//	$scope.$on('MyEvent', function() {
+//		$scope.count++;
+//	});
+//}
