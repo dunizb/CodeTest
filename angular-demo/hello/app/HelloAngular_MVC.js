86a4c5a74321b28276528ac0f19e7d12bebd1845
dunizb <dunizb@outlook.com>
2016-09-03 15:42:48 +0800

    angularjs-test-app

--- angular-demo/hello/app/HelloAngular_MVC.js
+++ angular-demo/hello/app/HelloAngular_MVC.js
@@ -1,5 +1,12 @@
-function HelloAngular($scope) {
+var myApp = angular.module("myApp",[]);
+myApp.controller("HelloAngular",["$scope",function($scope){
     $scope.greeting = {
         text: 'Hello'
     };
-}
+}])
+
+//function HelloAngular($scope) {
+//    $scope.greeting = {
+//        text: 'Hello'
+//    };
+//}
