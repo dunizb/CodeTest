12b6320dc268af2419b1151e6cc291bf9b8c942d
dunizb <dunizb@outlook.com>
2016-09-12 10:53:58 +0800

    nodejs web开发

---
+++ nodejs/web开发/01.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Administrator on 2016/9/11 0011.
+ */
+
+var http = require('http');
+
+//创建服务
+var server = http.createServer();
+
+//注册请求
+server.on('request',function(request,response){
+    console.log(123);
+    response.write('hellow write');
+    response.end();
+});
+
+//监听端口
+server.listen(3000);
