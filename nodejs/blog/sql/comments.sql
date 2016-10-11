97a43f8f2eea4d0c4857b81a1a5f943c1440c70a
dunizb <dunizb@outlook.com>
2016-10-11 14:22:09 +0800

    add sql

---
+++ nodejs/blog/sql/comments.sql
@@ -0,0 +1,48 @@
+﻿/*
+Navicat MySQL Data Transfer
+
+Source Server         : localTest
+Source Server Version : 50621
+Source Host           : localhost:3306
+Source Database       : blog
+
+Target Server Type    : MYSQL
+Target Server Version : 50621
+File Encoding         : 65001
+
+Date: 2016-10-11 14:17:02
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for comments
+-- ----------------------------
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE `comments` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `author` varchar(20) DEFAULT NULL,
+  `author_email` varchar(30) DEFAULT NULL,
+  `author_ip` varchar(255) DEFAULT NULL,
+  `content` varchar(500) DEFAULT NULL,
+  `status` varchar(30) DEFAULT NULL,
+  `support` int(11) DEFAULT '0' COMMENT '点赞数',
+  `oppose` int(11) DEFAULT '0' COMMENT '不点赞数',
+  `created` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '该条评论创建时间',
+  `post_id` bigint(20) DEFAULT NULL COMMENT '评论所属文章的id',
+  `parent_id` bigint(11) DEFAULT '0' COMMENT '父级评论的id',
+  `user_id` bigint(11) DEFAULT NULL COMMENT '评论者的id',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of comments
+-- ----------------------------
+INSERT INTO `comments` VALUES ('1', 'huoqishi', 'huoqishi@isc.net', '192.168.1.1', '小伙子写的不错哦！', null, '0', '0', '2016-10-08 19:28:27', '3', '0', '1');
+INSERT INTO `comments` VALUES ('2', 'sinopia', 'sinopia@tt.net', '192.168.1.199', '哈哈哈哈，加油!', null, '0', '0', '2016-10-08 19:57:39', '3', '0', null);
+INSERT INTO `comments` VALUES ('4', 'xiaoming', 'kk', '::ffff:127.0.0.1', '哈哈只', null, '0', '0', null, '4', '0', null);
+INSERT INTO `comments` VALUES ('5', 'xiaoming', 'kk', '::ffff:127.0.0.1', '哈哈只', null, '0', '0', null, '4', '0', null);
+INSERT INTO `comments` VALUES ('6', 'xiaoming', 'kk', '::ffff:127.0.0.1', '哈哈只', null, '0', '0', null, '4', '0', null);
+INSERT INTO `comments` VALUES ('7', 'xiaoming', 'kk', '::ffff:127.0.0.1', '哈哈只', null, '0', '0', null, '4', '0', null);
+INSERT INTO `comments` VALUES ('8', 'xiaoming', 'kk', '::ffff:127.0.0.1', '哈哈只', null, '0', '0', null, '4', '0', null);
+INSERT INTO `comments` VALUES ('9', 'q', 'f', '::ffff:127.0.0.1', 'a', null, '0', '0', null, '4', '0', null);
---
+++ nodejs/blog/sql/users.sql
@@ -0,0 +1,40 @@
+﻿/*
+Navicat MySQL Data Transfer
+
+Source Server         : localTest
+Source Server Version : 50621
+Source Host           : localhost:3306
+Source Database       : blog
+
+Target Server Type    : MYSQL
+Target Server Version : 50621
+File Encoding         : 65001
+
+Date: 2016-10-11 14:17:15
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `slug` varchar(30) NOT NULL DEFAULT '' COMMENT '用户别名',
+  `username` varchar(30) DEFAULT NULL COMMENT '用户名，登陆用的',
+  `password` varchar(30) DEFAULT NULL,
+  `nickname` varchar(30) DEFAULT NULL COMMENT '昵称',
+  `email` varchar(50) DEFAULT '' COMMENT '邮箱',
+  `status` varchar(30) DEFAULT NULL,
+  `created` datetime DEFAULT CURRENT_TIMESTAMP,
+  `role` varchar(255) DEFAULT NULL COMMENT '用户的角色',
+  `token` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+INSERT INTO `users` VALUES ('1', 'huoqishi', 'huoqishi', '123123', '火骑士空空', '', null, '2016-10-08 11:09:47', 'admin', null);
+INSERT INTO `users` VALUES ('2', 'tianyaren', 'tianyaren', '123223', '天涯人', '', null, '2016-10-09 07:41:53', '', null);
