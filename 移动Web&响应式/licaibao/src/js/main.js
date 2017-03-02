b5321b851ae026d576742f29de696e9177414cc6
dunizb <dunizb@outlook.com>
2017-03-02 22:24:27 +0800

    响应式图片1

--- 移动Web&响应式/licaibao/src/js/main.js
+++ 移动Web&响应式/licaibao/src/js/main.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-  $(".owl-carousel").owlCarousel();
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true
+    });
 });
