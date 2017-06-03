917d9932c4306e1fb787a4cde2bdf596e2bf4b15
dunizb <dunizb@outlook.com>
2017-06-03 21:58:49 +0800

    面向对象组件封装之权限配置组件2

--- JS面向对象/权限配置组件/assets/js/UserRuleSelect.js
+++ JS面向对象/权限配置组件/assets/js/UserRuleSelect.js
@@ -3,7 +3,8 @@ var UserRuleSelect = function(options) {
     this.render();//渲染DOM结构
 };
 
-var html = '<ul id="left-list" class="data-list">'
+var html = '<div class="user-rule-select">'
+            +'<ul class="left-list data-list">'
             +'<li>系统管理员</li>'
             +'<li>部门经理</li>'
             +'<li>部门主管</li>'
@@ -12,16 +13,18 @@ var html = '<ul id="left-list" class="data-list">'
             +'<li>财务出纳</li>'
         +'</ul>'
         +'<div class="data-oper">'
-            +'<a href="#" id="add" class="button">添加</a>'
-            +'<a href="#" id="del" class="button">删除</a>'
+            +'<a href="#" class="add button">添加</a>'
+            +'<a href="#" class="del button">删除</a>'
         +'</div>'
-        +'<ul id="right-list" class="data-list"></ul>';
+        +'<ul class="right-list data-list"></ul>'
+        +'<div class="data-bar"><a href="#" class="close button">取消</a><a href="#" class="enter button">确定</a></div>'
+        +'</div>';
 
 UserRuleSelect.prototype = {
     init: function(options) {
         this.options = options; //初始化参数
         this.dom = document.createElement('div');
-        this.dom.className = 'container user-rule-select';
+        this.dom.className = 'mask';
         this.dom.style.display = this.options.defaultShow ? 'flex' : 'none';//由用户决定是否初始化显示
         this.status = this.options.defaultShow ? 0 : 1;//0隐藏、1显示
         document.body.appendChild(this.dom);
