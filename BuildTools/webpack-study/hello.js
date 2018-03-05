771f95e501b4aaf8dcbbba3c7971d0cfbdef64d9
dunizb <dunizb@outlook.com>
2018-03-05 21:59:19 +0800

    webpack3
    多入口配置

--- BuildTools/webpack-study/hello.js
+++
@@ -1,6 +0,0 @@
-require('style-loader!css-loader!./public.css');
-
-function hello(){
-    alert("hello webpack!")
-}
-hello();
---
+++ BuildTools/webpack3/src/entry.js
@@ -0,0 +1,1 @@
+document.getElementById("title").innerHTML = "Hello Dunizb.com";
