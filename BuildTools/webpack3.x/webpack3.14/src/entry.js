99a2bea368c76133f6df0ba2a847d22997f0c0a2
dunizb <dunizb@outlook.com>
2018-03-11 19:01:28 +0800

    del webpack3.14

--- BuildTools/webpack3.x/webpack3.14/src/entry.js
+++
@@ -1,7 +0,0 @@
-import css from './css/index.css';
-import less from './css/black.less';
-import sass from './css/nav.scss';
-
-const dunizb = "www.dunizb.com"
-
-document.getElementById("title").innerHTML = `Loders PostCSS ${dunizb}`;
