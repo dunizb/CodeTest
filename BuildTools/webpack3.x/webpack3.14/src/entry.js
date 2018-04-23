2da873ce548016cf19c303cd266f83626999ffef
dunizb <dunizb@outlook.com>
2018-04-23 12:36:59 +0800

    开发和生产并行设置

---
+++ BuildTools/webpack3.x/webpack3.14/src/entry.js
@@ -0,0 +1,7 @@
+import css from './css/index.css';
+import less from './css/black.less';
+import sass from './css/nav.scss';
+
+const dunizb = "www.dunizb.com"
+
+document.getElementById("title").innerHTML = `Loders PostCSS ${dunizb}`;
