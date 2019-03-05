22e8c9af0c0a436b7aac5ee7f4b6d01cf7bee4d1
Dunizb <zb@dunizb.vip>
2019-03-05 21:55:16 +0800

    bubbleSort.html

--- JavaScript/工具函数/src/index.js
+++ JavaScript/工具函数/src/index.js
@@ -1,40 +1,6 @@
 const Utils = {
     strings: {
         /**
-         * 数组去重(双重循环)
-         * @param {Array} array 
-         * @return {Array} 去重后的数组
-         */
-        unique: function (array) {
-            if (!Array.isArray(array)) {
-                throw TypeError('参数不是一个数组')
-            }
-            const newArray = []
-            let isRepeat
-            for (let i = 0; i < array.length; i++) {
-                isRepeat = false
-                for (let j = 0; j < newArray.length; j++) {
-                    if (array[i] === newArray[j]) {
-                        isRepeat = true
-                        break
-                    }
-                }
-                if (!isRepeat) newArray.push(array[i])
-            }
-            return newArray
-        },
-        /**
-         * 数组去重(ES6语法)
-         * @param {Array} array 
-         * @return {Array} 去重后的数组
-         */
-        uniquees6: function (array) {
-            if (!Array.isArray(array)) {
-                throw TypeError('参数不是一个数组')
-            }
-            return [...new Set(array)]
-        },
-        /**
          * 判断一个单词是否是回文
          * @param {String} str 
          * @return {Boolean}
@@ -88,5 +54,61 @@ const Utils = {
             return upper ? temp.toUpperCase() : temp
         }
     },
-    arrays: {}
+    arrays: {
+        /**
+         * 数组去重(双重循环)
+         * @param {Array} array 
+         * @return {Array} 去重后的数组
+         */
+        unique: function (array) {
+            if (!Array.isArray(array)) {
+                throw TypeError('参数不是一个数组')
+            }
+            const newArray = []
+            let isRepeat
+            for (let i = 0; i < array.length; i++) {
+                isRepeat = false
+                for (let j = 0; j < newArray.length; j++) {
+                    if (array[i] === newArray[j]) {
+                        isRepeat = true
+                        break
+                    }
+                }
+                if (!isRepeat) newArray.push(array[i])
+            }
+            return newArray
+        },
+        /**
+         * 数组去重(ES6语法)
+         * @param {Array} array 
+         * @return {Array} 去重后的数组
+         */
+        uniquees6: function (array) {
+            if (!Array.isArray(array)) {
+                throw TypeError('参数不是一个数组')
+            }
+            return [...new Set(array)]
+        },
+        /**
+         * 冒泡排序
+         * @param {Array} array 排序前的数组
+         * @return {Array} array 排序后的数组
+         */
+        bubbleSort (array) {
+            if (!Array.isArray(array)) {
+                throw TypeError('参数不是一个数组')
+            }
+            let len = array.length
+            for (let i = 0; i < len; i++) {
+                for (let j = 0; j < len - 1 - i; j++ ) {
+                    if (array[j] > array[j + 1]) { // 相邻元素两两对比
+                        let temp = array[j + 1] // 元素交换
+                        array[j + 1] = array[j]
+                        array[j] = temp
+                    }
+                }
+            }
+            return array
+        }
+    }
 }
