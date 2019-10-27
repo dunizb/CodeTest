c049e32afa4b44d661ddb4d4049a8f8fca373477
Dunizb <zb@dunizb.vip>
2019-10-27 23:36:55 +0800

    观察者模式

--- 设计模式/观察者设计模式/观察者模式1.js
+++ 设计模式/观察者设计模式/观察者模式1.js
@@ -2,7 +2,6 @@
  * 简单的观察者模式，发布固定的消息
  * 给学生发布一些消息
  ************************/
-console.log('======【简单的观察者模式】======')
 
 // 1. 消息的发布者
 // 2. 可以让别人订阅你的消息
@@ -24,8 +23,8 @@ const lk = {
     }
 }
 
-function response() {
-    console.log('消息已推送.', this.name);
+function response(msg) {
+    console.log('消息已推送给【', this.name, '】，消息为：', msg);
 }
 
 let stu1 = { name: '张三' }
