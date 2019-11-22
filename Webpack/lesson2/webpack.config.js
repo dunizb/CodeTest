bf0c5be1f59c1e98e548fcf2a1c8dd6d65e0d41d
Dunizb <zb@dunizb.vip>
2019-11-23 01:11:38 +0800

    babel-loader options

--- Webpack/lesson2/webpack.config.js
+++ Webpack/lesson2/webpack.config.js
@@ -26,7 +26,10 @@ module.exports = {
             { 
                 test: /\.js$/, 
                 exclude: /node_modules/, 
-                loader: "babel-loader" 
+                loader: "babel-loader",
+                options: {
+                    "presets": ["@babel/preset-env"]
+                } 
             },
             {
                 test: /\.(jpg|png|gif)$/,
