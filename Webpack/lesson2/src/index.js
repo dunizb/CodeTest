52b612eb770dda7c37ec8d44946a8764172f4f65
Dunizb <zb@dunizb.vip>
2019-11-22 23:24:35 +0800

    HtmlWebpackPlugin,CleanWebpackPlugin,devtool: 'cheap-module-eval-source-map'

---
+++ Webpack/lesson2/src/index.js
@@ -0,0 +1,6 @@
+import './index.scss'
+
+const dom = document.getElementById('root')
+dom.innerHTML = '<div class="calcimg">HtmlWebpackPlugin</div>'
+
+consoles.log('hello world')
---
+++ Webpack/lesson2/webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    devtool: 'cheap-module-eval-source-map',
+    entry: {
+        main: './src/index.js'
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules:[
+            {
+                test: /\.(jpg|png|gif)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        name: '[name]_[hash].[ext]',
+                        outputPath: 'images',
+                        limit: 2048
+                    }
+                }
+            },{
+                test: /\.scss$/,
+                use: [
+                    "style-loader", // 将 JS 字符串生成为 style 节点
+                    {
+                        loader: "css-loader", // 将 CSS 转化成 CommonJS 模块
+                        options: {
+                            importLoaders: 2,
+                            modules: true // 开启模块化的CSS 
+                        }
+                    },
+                    "sass-loader", // 将 Sass 编译成 CSS，默认使用 Node Sass
+                    "postcss-loader"
+                ],
+            }
+        ]
+    },
+    plugins: [
+        // 打包后运行
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        }),
+        // 打包前清理dist目录
+        new CleanWebpackPlugin()
+    ]
+}
+
