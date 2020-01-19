fce3fc0f434b50346ad6773f34a02c9361a02f5f
Dunizb <zb@dunizb.vip>
2020-01-19 18:30:13 +0800

    init 2

---
+++ React/antd-creat-ms/src/pages/demo/life.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default class Life extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            count: 0
+        }
+    }
+    handleAdd = () => {
+        this.setState({
+            count: this.state.count + 1
+        })
+    }
+    render() {
+        return <div>
+            <h1>React 生命周期</h1>
+            <button onClick={this.handleAdd}>点击一下</button>
+            <p>{this.state.count}</p>
+        </div>
+    }
+}
