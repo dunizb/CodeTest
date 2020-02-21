aeb86b789773653703a647f05f3537187f2d0a1c
Dunizb <zb@dunizb.vip>
2020-02-21 23:50:19 +0800

    React-Router-Demo

---
+++ docs/project/React-Router-Demo/precache-manifest.4e5ecef38b1eb2cf367aeb18fdcc5f27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e9b8b7130f92163b025a11a400b50c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1997646cbe7e827acff",
+    "url": "/static/css/main.2fdc6e33.chunk.css"
+  },
+  {
+    "revision": "85769b97c902dd9c8d0e",
+    "url": "/static/js/2.1c610aac.chunk.js"
+  },
+  {
+    "revision": "c1997646cbe7e827acff",
+    "url": "/static/js/main.29d004c2.chunk.js"
+  },
+  {
+    "revision": "ca26963c7f36c591c891",
+    "url": "/static/js/runtime-main.882d2b91.js"
+  }
+]);
