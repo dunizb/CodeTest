2447fd3710446351d385b9043374667c9e4d109f
Dunizb <zb@dunizb.vip>
2020-02-21 23:35:14 +0800

    react-redux-todolist 优化

--- React/redux/react-redux-todolist/src/TodoList.js
+++ React/redux/react-redux-todolist/src/TodoList.js
@@ -1,38 +1,32 @@
-import React, { PureComponent } from 'react';
-import store from './store'
-import {connect} from 'react-redux'
+import React from 'react';
+import {connect} from 'react-redux';
 import {
     changeInputAction, 
     addItemAction
 } from './store/actionCreatores'
 
-class TodoList extends PureComponent {
-    constructor(props) {
-        super(props);
-        this.state = store.getState()
-    }
-    render() { 
-        return (
-            <div style={{margin:'10px', width: '600px'}}>
-                <h1>react-redux-todolist</h1>
-                <div style={{marginBottom:'10px'}}>
-                    <div>
-                        <input value={this.props.inputValue} onChange={this.props.inputChange} />
-                        <button onClick={this.props.clickButton}>提交</button>
-                    </div>
-                    <ul>
-                        {
-                            this.props.list.map((item,index)=>{
-                                return (<li key={index}>{item}</li>)
-                            })
-                        }
-                    </ul>
+const TodoList = (props) => {
+    const { inputValue, inputChange, clickButton, list } = props
+    return (
+        <div style={{margin:'10px', width: '600px'}}>
+            <h1>react-redux-todolist</h1>
+            <div style={{marginBottom:'10px'}}>
+                <div>
+                    <input value={inputValue} onChange={inputChange} />
+                    <button onClick={clickButton}>提交</button>
                 </div>
-                <hr />
-                <a href="https://github.com/dunizb/CodeTest/tree/master/React/redux/react-redux-todolist" target="_blank">查看源代码</a>     
+                <ul>
+                    {
+                        list.map((item,index)=>{
+                            return (<li key={index}>{item}</li>)
+                        })
+                    }
+                </ul>
             </div>
-        );
-    }
+            <hr />
+            <a href="https://github.com/dunizb/CodeTest/tree/master/React/redux/react-redux-todolist" rel="noopener noreferrer" target="_blank">查看源代码</a>     
+        </div>
+    );
 }
 
 const stateToProps = (state) => {
