db50060ea20dc911dd6a056a943e3e0378756328
Dunizb <zb@dunizb.vip>
2020-02-21 17:57:12 +0800

    add build

---
+++ React/redux/React-and-Redux-TodoList/build/precache-manifest.01fc84b35ca436507c4b0fa0f15de7a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "156224d25ab8ee421ea7790f647627dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bc8a70db6e74f1190a6",
+    "url": "/static/css/2.5bc4965f.chunk.css"
+  },
+  {
+    "revision": "7bc8a70db6e74f1190a6",
+    "url": "/static/js/2.4b2acd0c.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/static/js/2.4b2acd0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b373c9d054047476006f",
+    "url": "/static/js/main.6a4559b8.chunk.js"
+  },
+  {
+    "revision": "ab2d64092d801783aa33",
+    "url": "/static/js/runtime-main.0ddd433a.js"
+  }
+]);
