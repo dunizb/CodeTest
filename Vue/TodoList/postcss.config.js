ad33c3be21269063fe90e14d5f71a90916e4e690
Dunizb <zb@dunizb.vip>
2020-02-22 19:26:07 +0800

    delete todolist

--- Vue/TodoList/postcss.config.js
+++
@@ -1,9 +0,0 @@
-const autoprefixer = require('autoprefixer');
-
-// stylus编译成css后，再优化css代码
-// autoprefixer 给CSS代码自动添加不同浏览器的前缀
-module.exports = {
-    plugins: [
-        autoprefixer()
-    ]
-};
--- Vue/TodoList/src/components/notification/index.js
+++
@@ -1,7 +0,0 @@
-import Notification from './notification.vue';
-import notify from './function.js';
-
-export default (Vue) => {
-  Vue.component(Notification.name, Notification);
-  Vue.prototype.$notify = notify; // this.$notify()
-}
