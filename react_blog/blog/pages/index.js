192f16bd04c3b1903bb93ea4977f6aea2e3f9227
Dunizb <zb@dunizb.vip>
2020-02-23 17:04:35 +0800

    create project

---
+++ react_blog/blog/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+const Home = () => (
+  <div className="container">
+    <Head>
+      <title>Create Next App</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  </div>
+)
+
+export default Home
