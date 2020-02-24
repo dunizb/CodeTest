f4a343831a5e84e99280e37f481b3e5d7a212bb3
Dunizb <zb@dunizb.vip>
2020-02-24 22:57:55 +0800

    update docs ReactHook

---
+++ docs/project/ReactHook/precache-manifest.61ab3832c471560560f97f22a26184a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1e58ddf79b453ae4606acc5020e55f8",
+    "url": "/CodeTest/project/ReactHook/index.html"
+  },
+  {
+    "revision": "384639f0e011c321304b",
+    "url": "/CodeTest/project/ReactHook/static/css/main.ffffbba5.chunk.css"
+  },
+  {
+    "revision": "45da645ccdd958cd722f",
+    "url": "/CodeTest/project/ReactHook/static/js/2.b8ae0d6a.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/CodeTest/project/ReactHook/static/js/2.b8ae0d6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "384639f0e011c321304b",
+    "url": "/CodeTest/project/ReactHook/static/js/main.fed37f12.chunk.js"
+  },
+  {
+    "revision": "ab1b820fa45bd3b50271",
+    "url": "/CodeTest/project/ReactHook/static/js/runtime-main.5c366bbe.js"
+  }
+]);
--- docs/project/ReactHook/service-worker.js
+++ docs/project/ReactHook/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b415e5998a647bbe55f39a666d5bd571.js"
+  "/CodeTest/project/ReactHook/precache-manifest.61ab3832c471560560f97f22a26184a0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CodeTest/project/ReactHook/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
