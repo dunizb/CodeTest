a8fa199a3b227aa9dea6525d080c2f2fcd4c77c8
Dunizb <zb@dunizb.vip>
2020-03-23 17:51:39 +0800

    api getArticleList

--- projects/react_blog/egg-server/app/controller/blog/home.js
+++ projects/react_blog/egg-server/app/controller/blog/home.js
@@ -5,8 +5,22 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
-    const result = await this.app.mysql.get('blog_content', {});
-    ctx.body = result;
+    ctx.body = 'api hi';
+  }
+
+  async getArticleList() {
+    const { ctx } = this;
+    const sql = `
+    select 
+    article.id,article.title , article.content, article.type_id,article.introduce,article.addTime,article.view_count,
+		article_type.typeName
+    from article 
+    LEFT JOIN article_type 
+    ON article_type.id = article.type_id
+    `;
+
+    const results = await this.app.mysql.query(sql);
+    ctx.body = { data: results };
   }
 }
 
--- projects/react_blog/egg-server/app/router/blog.js
+++ projects/react_blog/egg-server/app/router/blog.js
@@ -3,4 +3,5 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/blog/index', controller.blog.home.index);
+  router.get('/blog/getArticleList', controller.blog.home.getArticleList);
 };
