86a836bed4893148f2b35e84cc394f56b0834cb8
Dunizb <zb@dunizb.vip>
2020-05-19 12:26:34 +0800

    create admin

---
+++ projects/react_blog/admin/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'antd';
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary">Button</Button>
+    </div>
+  );
+}
+
+export default App;
