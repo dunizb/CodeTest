b63fb6fae01b5fe8230b06a614eec08b161b671e
Dunizb <zb@dunizb.vip>
2020-06-01 23:00:45 +0800

    add uni-app-todo and layput card

---
+++ projects/uni-app-todo/pages/index/index.vue
@@ -0,0 +1,152 @@
+<template>
+	<view class="content">
+		<view class="todo-header">
+      <!-- 状态栏左侧 -->
+		  <view class="todo-header__left">
+		    <text class="active-text">全部</text>
+        <text>10条</text>
+		  </view>
+      <!-- 状态栏右侧 -->
+      <view class="todo-header__right">
+        <view class="todo-header__right-item active-tab">全部</view>
+        <view class="todo-header__right-item">待办</view>
+        <view class="todo-header__right-item">已完成</view>
+      </view>
+		</view>
+    <view class="todo-context">
+      <view class="todo-list todo--finish">
+        <view class="todo-list__checkbox">
+          <view class="checkbox"></view>
+        </view>
+        <view class="todo-list__context">
+          我的待办事项
+        </view>
+      </view>
+      <view class="todo-list">
+        <view class="todo-list__checkbox">
+          <view class="checkbox"></view>
+        </view>
+        <view class="todo-list__context">
+          我的待办事项
+        </view>
+      </view>
+    </view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.todo-header{
+    display: flex;
+    justify-content: space-between;
+    align-items:center;
+    padding: 0 15px;
+    font-size: 12px;
+    color: #333333;
+    height: 45px;
+    box-shadow: -1px 1px 5px 0px rgba(0,0,0,.1);
+    background-color: #FFFFFF;
+  }
+  todo-header__left{
+    width: 100%;
+  }
+  .todo-header__right{
+    display: flex;
+  }
+  .todo-header__right-item {
+    padding: 0 5px;
+  }
+  .active-text {
+    font-size: 14px;
+    color: #279abf;
+    padding-right: 10px;
+  }
+  .active-tab {
+    color: #279abf;
+  }
+  .todo-context {
+    position: relative;
+  }
+  .todo-list{
+    position: relative;
+    display: flex;
+    align-items: center;
+    padding: 15px;
+    margin: 15px;
+    color: #0c3854;
+    font-size: 14px;
+    border-radius: 10px;
+    background-color: #cfebfd;
+    box-shadow: -1px 1px 5px 1px rgba(0,0,0,.1), -1px 2px 1px 0 rgb(255,255,255) inset;
+    overflow: hidden;
+  }
+  .todo-list::after{
+    position: absolute;
+    content: "";
+    top: 0;
+    bottom: 0;
+    left: 0;
+    width: 5px;
+    background-color: #91d1e8;
+  }
+  .todo-list__checkbox{
+    padding-right: 15px;
+  }
+  .checkbox {
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: #FFFFFF;
+    box-shadow: 0 0 5px 1px rgba(0,0,0,.1);
+  }
+  .todo--finish .checkbox{
+    position: relative;
+    background-color: #eee;
+  }
+  .todo--finish .checkbox::after{
+    content: "";
+    position: absolute;
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    margin: auto;
+    background-color: #CFEBFD;
+    box-shadow: 0 0 2px 0 rgba(0,0,0,.2) inset;
+  }
+  .todo--finish .todo-list__context{
+    color: #999999;
+  }
+  .todo--finish.todo-list::before{
+    content: "";
+    position: absolute;
+    top: 50%;
+    left: 40px;
+    right: 10px;
+    height: 2px;
+    margin: 0 auto;
+    transform: translateY(-50%);
+    background-color: #bdcdd8;
+  }
+  .todo--finish.todo-list::after{
+    background-color: #ccc;
+  }
+</style>
