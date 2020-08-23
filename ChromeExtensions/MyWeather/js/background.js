29967c63c2fd547974c9d9509ce849b636a727e2
Dunizb <zb@dunizb.vip>
2020-08-23 18:41:17 +0800

    update MyWeather

--- ChromeExtensions/MyWeather/js/background.js
+++ ChromeExtensions/MyWeather/js/background.js
@@ -1,32 +1,38 @@
-function pFetch(cityName) {
+function pFetch() {
   return new Promise((resolve, reject) => {
-    fetch(`http://wthrcdn.etouch.cn/weather_mini?city=${cityName}`)
+    fetch(
+      "https://tianqiapi.com/api?version=v6&appid=24222488&appsecret=5rwuHzOJ",
+    )
       .then((res) => res.json())
       .then((data) => {
-        if (data.status === 1000) {
-          resolve(data.data);
-        } else {
-          reject(data.desc);
-        }
+        resolve(data);
       }).catch((err) => {
         reject(err);
       });
   });
 }
 
+/**
+ * 设置扩展图标
+ * @param {String} iconName 天气图片名称
+ */
 function setIcon(iconName) {
   chrome.browserAction.setIcon({
     path: {
-      "128": `images/${iconName}.png`,
+      "128": `http://tq.daodaoim.com/tianqiapi/skin/pitaya/${iconName}.png`,
     },
   });
 }
 
-function sendNotification(cityName, iconName) {
-  const notification = new Notification(`${cityName}天气提醒`, {
-    body: `${cityName}的天气有新变化了，快去查看吧`,
-    tag: "daxin",
-    icon: `images/${iconName}.png`,
+/**
+ * 发送桌面通知
+ * @param {String} city 城市名称
+ * @param {String} iconName 天气图片
+ */
+function sendNotification(city, iconName) {
+  const notification = new Notification(`${city}天气提醒`, {
+    body: `${city}的天气有新变化了，快去查看吧`,
+    icon: `http://tq.daodaoim.com/tianqiapi/skin/pitaya/${iconName}.png`,
     requireInteraction: true,
   });
   setTimeout(() => {
@@ -35,24 +41,26 @@ function sendNotification(cityName, iconName) {
 }
 
 function init() {
-  let cityName = localStorage.getItem("local_city") || "北京";
   // 上一次天气状态
-  let todayType = localStorage.getItem("today_type");
+  let preType = localStorage.getItem("pre_type");
   const timerId = setInterval(() => {
-    pFetch(cityName).then((data) => {
-      const type = data.forecast[0]["type"]; // 晴，多云，小雨...
-      if (todayType !== type) {
-        chrome.browserAction.setBadgeText({ text: type });
+    pFetch().then((data) => {
+      console.log(data);
+      const wea = data.wea; // 天气，如晴，多云等
+      // 和上一次天气不同，天气有变化
+      if (preType !== wea) {
+        // 设置扩展图标Badge
+        chrome.browserAction.setBadgeText({ text: wea });
         chrome.browserAction.setBadgeBackgroundColor(
           { color: [255, 0, 0, 255] },
         );
         // 发送桌面通知
-        sendNotification(cityName, type);
-        todayType = type;
+        sendNotification(data.city, wea);
       }
-      localStorage.setItem("today_type", todayType);
+      // 保存这一次天气状态
+      localStorage.setItem("pre_type", wea);
       // 修改扩展图标
-      setIcon(todayType);
+      setIcon(data.wea_img);
     });
   }, 1000 * 60);
 }
